657126e4b84f86653d127be16b626119c100ebc3
LucasSimoesSilva <lucassimoes.carvalho@gmail.com>
2025-11-08 19:21:53 -0300

    fix: correct file path reference

--- tests/collisions_test.py
+++ tests/collisions_test.py
@@ -1,5 +1,5 @@
 from cluster_cvd_step import *
 
 centroids_lab, pal_cvd_lab, D_cvd, edges = analyze_collisions(
-    "images/ishiharaTest.png", cluster_number=6, cvd_type="deutan", severity=1.0, threshold=8.0
+    "../images/ishiharaTest.png", cluster_number=6, cvd_type="deutan", severity=1.0, threshold=8.0
 )
